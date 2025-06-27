ba8870fadf1bbf164d09c29cbea80cedc6eaa057
Delfina Munich <delfina.munich@machineslikeme.com>
2025-06-26 21:27:48 -0300

    intentando swagger

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import express from 'express'
 import productoRouter from './route/producto.router.js'
+import YAML from 'yamljs'
+import swaggerUi from 'swagger-ui-express'
 
 
 const app = express()
+const swaggerDocument = YAML.load("./openapi.yaml");
 const PORT = 8080
 app.use (express.json())
 app.use ("/productos", new productoRouter().start())
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
 app.use ((req,res) => {
     res.status(404).json({
